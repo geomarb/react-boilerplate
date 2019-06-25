7a9ec6131900aae8a1ded03873b6eb1e1f298868
geomarb <gtcbrs@gmail.com>
2019-06-25 16:38:36 +0100

    Init commit

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,26 @@
+// o que esse cara faz:
+// cria as rotas a serem executadas apenas no browser, sem fazer request no server ao trocar de pagina
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import LoginPage from '../components/LoginPage';
+import DashboardPage from '../components/DashboardPage';
+import NotFoundPage from '../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+export const history = createHistory();
+
+const AppRouter = () => (
+  <Router history={history}>
+      <div>
+          <Switch>
+              <PublicRoute path="/" component={LoginPage} exact />
+              <PrivateRoute path="/dashboard" component={DashboardPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </Router>
+);
+
+export default AppRouter;
